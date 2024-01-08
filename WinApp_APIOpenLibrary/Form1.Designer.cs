1b17904ed2b6a3cbf7b9f5e88de1f7a565eab2d4
Erick Joel Malan <erickjpk1234@gmail.com>
2024-01-08 15:45:18 -0500

    Agregar archivos de proyecto.

---
+++ WinApp_APIOpenLibrary/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WinApp_APIOpenLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lato", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(657, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OpenLibrary Api (Open Library Search API)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lato", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(97, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Buscar autor(es) de libro:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(341, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Lato", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(220, 227);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(350, 256);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lato", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(345, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Autores:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 537);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Lato", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WinApp_APIOpenLibrary/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WinApp_APIOpenLibrary
+{
+    public partial class Form1 : Form
+    {
+        const string ApiBaseUrl = "https://openlibrary.org/search.json";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                buscar(textBox1.Text);
+
+            }
+        }
+
+        private async void buscar(string libro)
+        {
+            string libroBuscado = libro;
+
+            if (!string.IsNullOrWhiteSpace(libroBuscado))
+            {
+                try
+                {
+                    string apiUrl = $"{ApiBaseUrl}?q={libroBuscado.Replace(" ", "+")}";
+                    string jsonResponse = await GetApiResponse(apiUrl);
+
+                    JObject result = JObject.Parse(jsonResponse);
+
+                    if (result.ContainsKey("docs"))
+                    {
+                        JArray docs = (JArray)result["docs"];
+                        if (docs.Count > 0)
+                        {
+                            HashSet<string> autoresSet = new HashSet<string>(); // Usamos un HashSet para evitar duplicados
+
+                            for (int i = 0; i < docs.Count - 1; i++)
+                            {
+                                if (docs[i]["author_name"] != null && docs[i]["author_name"].Any())
+                                {
+                                    string autor = string.Join(", ", docs[i]["author_name"]);
+                                    autoresSet.Add(autor );
+                                }
+                            }
+
+                            // Agregar los autores al listBox1
+                            foreach (string autor in autoresSet)
+                            {
+                                listBox1.Items.Add(autor);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se encontraron autores.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Respuesta no presenta 'docs', es decir no se ha devuelto ningún libro ni autor.");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese el nombre del libro.");
+            }
+        }
+
+        private async Task<string> GetApiResponse(string apiUrl)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
